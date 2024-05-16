b3ce4eeea7afe21a8517dbe0b351a24e50ee15e9
Randy Syring <randy@syrings.us>
2024-05-16 16:56:17 -0400

    initial commit after copyier-py-package generation

---
+++ noxfile.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import nox
+
+
+package_path = Path.cwd()
+
+# NOTE: uv is much faster at creating venvs and generally compatible with pip.
+# Pip compat: https://github.com/astral-sh/uv/blob/main/PIP_COMPATIBILITY.md
+nox.options.default_venv_backend = 'uv'
+
+
+@nox.session
+def tests(session: nox.Session):
+    session.install('-r', 'requirements/base.txt')
+    session.run(
+        'pytest',
+        # use our pytest.ini for warning management
+        '-c=ci/pytest.ini',
+        '-ra',
+        '--tb=native',
+        '--strict-markers',
+        '--cov=pre_commit_hooks',
+        '--cov-config=.coveragerc',
+        '--cov-report=xml',
+        '--no-cov-on-fail',
+        f'--junit-xml={package_path}/ci/test-reports/{session.name}.pytests.xml',
+        'src',
+    )
+
+
+@nox.session
+def standards(session: nox.Session):
+    session.install('-c', 'requirements/dev.txt', 'pre-commit')
+    session.run(
+        'pre-commit',
+        'run',
+        '--show-diff-on-failure',
+        '--all-files',
+    )
